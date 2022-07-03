5bf05ba173aa83415af7e0ccc76327f8609368a0
SashkaDikaya <dikayaav@gmail.com>
2022-07-03 22:49:14 +0300

    some changes

---
+++ simple_tests.py
@@ -0,0 +1,23 @@
+def test_0():
+    assert 2 > 1
+
+
+
+def test_one():
+    assert 2 > 1
+
+
+def test_two():
+    assert 3 > 1
+
+
+def test_3():
+    assert 3 > 1
+
+
+def test_6():
+    assert 2 > 1
+
+
+def test_7():
+    print("Hello")
